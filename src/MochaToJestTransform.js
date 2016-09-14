c36b4c94ec84a6b7ee5a5ae8843d0a3dacc2e146
Andrew Hutchings <andrew.hutchings@gmail.com>
2016-09-13 21:49:07 -0400

    Configure linter

--- src/MochaToJestTransform.js
+++ src/MochaToJestTransform.js
@@ -1,51 +1,51 @@
 export default (file, api, options) => {
-  const j = api.jscodeshift;
-  const root = j(file.source);
+  const j = api.jscodeshift
+  const root = j(file.source)
 
-  const printOptions = options.printOptions || {quote: 'single'};
+  const printOptions = options.printOptions || {quote: 'single'}
 
   const didUpdateSpecFunctionExpressions = root
     .find(j.FunctionExpression)
     .filter(isSpecFunctionExpressionPath)
     .forEach(replaceFunctionExpressionWithArrowFunctionExpression)
-    .size() > 0;
+    .size() > 0
 
-  function replaceFunctionExpressionWithArrowFunctionExpression(node) {
+  function replaceFunctionExpressionWithArrowFunctionExpression (node) {
     j(node).replaceWith(
       j.arrowFunctionExpression([], node.value.body)
-    );
+    )
   }
 
   const didUpdateItOnlyExpressions = root
     .find(j.MemberExpression)
     .filter(isItOnlyExpressionPath)
     .forEach(replaceItOnlyExpression)
-    .size() > 0;
+    .size() > 0
 
-  function replaceItOnlyExpression(node) {
+  function replaceItOnlyExpression (node) {
     j(node).replaceWith(
       j.identifier('fit')
-    );
+    )
   }
 
   const didUpdate = (
     didUpdateSpecFunctionExpressions ||
     didUpdateItOnlyExpressions
-  );
+  )
 
-  return didUpdate ? root.toSource(printOptions) : null;
+  return didUpdate ? root.toSource(printOptions) : null
 }
 
-function isSpecFunctionExpressionPath(path) {
+function isSpecFunctionExpressionPath (path) {
   return (
     path.parentPath.name === 'arguments' &&
     path.parentPath.parentPath.value.type === 'CallExpression' &&
     isDescribeOrItBlock(path.parentPath.parentPath.value.callee)
-  );
+  )
 }
 
-function isDescribeOrItBlock(node) {
-  return isAnyDescribeBlock(node) || isAnyItBlock(node);
+function isDescribeOrItBlock (node) {
+  return isAnyDescribeBlock(node) || isAnyItBlock(node)
 }
 
 const isAnyDescribeBlock = node =>
@@ -55,15 +55,13 @@ const isAnyItBlock = node =>
   isItIdentifier(node) ||
   isItOnlyExpression(node)
 
-const isSpec = node => node.type === 'FunctionExpression'
-
 const isDescribeIdentifier = node =>
   node.type === 'Identifier' && node.name === 'describe'
 
 const isItIdentifier = node =>
   node.type === 'Identifier' && node.name === 'it'
 
-const isItOnlyExpressionPath = path => isItOnlyExpression(path.value);
+const isItOnlyExpressionPath = path => isItOnlyExpression(path.value)
 
 const isItOnlyExpression = node => {
   return node.type === 'MemberExpression' &&
@@ -72,4 +70,4 @@ const isItOnlyExpression = node => {
 }
 
 const isOnlyIdentifier = node =>
-  node.type === 'Identifier' && node.name === 'only';
+  node.type === 'Identifier' && node.name === 'only'
--- src/__tests__/MochaToJestTransform-test.js
+++ src/__tests__/MochaToJestTransform-test.js
@@ -1,5 +1,5 @@
-import {defineTest} from 'jscodeshift/dist/testUtils';
+import {defineTest} from 'jscodeshift/dist/testUtils'
 
-defineTest(__dirname, 'MochaToJestTransform', null, 'DescribeBlock');
-defineTest(__dirname, 'MochaToJestTransform', null, 'ItBlock');
-defineTest(__dirname, 'MochaToJestTransform', null, 'ItOnlyBlock');
+defineTest(__dirname, 'MochaToJestTransform', null, 'DescribeBlock')
+defineTest(__dirname, 'MochaToJestTransform', null, 'ItBlock')
+defineTest(__dirname, 'MochaToJestTransform', null, 'ItOnlyBlock')
